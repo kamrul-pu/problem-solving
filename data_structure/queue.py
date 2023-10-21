5432b02509cea1d583dd2b4ecf15fbce4ea69539
Kamrul Hasan <kamrul.h456@gmail.com>
2023-10-21 13:46:20 +0600

    validation for empty queue

--- data_structure/queue.py
+++ data_structure/queue.py
@@ -9,12 +9,13 @@ class Queue:
         self.buffer.appendleft(val)
 
     def dequeue(self):
-        return self.buffer.pop()
+        if not self.is_empty():
+            return self.buffer.pop()
 
-    def is_empty(self):
+    def is_empty(self) -> bool:
         return len(self.buffer) == 0
 
-    def size(self):
+    def size(self) -> int:
         return len(self.buffer)
 
 
