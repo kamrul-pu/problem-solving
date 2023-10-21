116fafcdc582c72a0efd708169122288445479de
Kamrul Hasan <kamrul.h456@gmail.com>
2023-10-21 13:45:09 +0600

    Queue Data structure

---
+++ data_structure/queue.py
@@ -0,0 +1,48 @@
+from collections import deque
+
+
+class Queue:
+    def __init__(self) -> None:
+        self.buffer = deque()
+
+    def enqueue(self, val):
+        self.buffer.appendleft(val)
+
+    def dequeue(self):
+        return self.buffer.pop()
+
+    def is_empty(self):
+        return len(self.buffer) == 0
+
+    def size(self):
+        return len(self.buffer)
+
+
+pq = Queue()
+
+pq.enqueue(
+    {
+        "company": "Wall Mart",
+        "timestamp": "15 apr, 11.01 AM",
+        "price": 131.10,
+    }
+)
+pq.enqueue(
+    {
+        "company": "Wall Mart",
+        "timestamp": "15 apr, 11.02 AM",
+        "price": 132,
+    }
+)
+pq.enqueue(
+    {
+        "company": "Wall Mart",
+        "timestamp": "15 apr, 11.03 AM",
+        "price": 135,
+    }
+)
+print(pq.size())
+
+print(pq.buffer)
+
+print(pq.dequeue())
