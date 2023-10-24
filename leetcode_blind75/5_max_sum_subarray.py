c9836b3e200f70453d076fc5bdb25acb760d5d69
Kamrul Hasan <kamrul.h456@gmail.com>
2023-10-25 00:11:49 +0600

    maximum sum subarray in O(n)

--- leetcode_blind75/5_max_sum_subarray.py
+++ leetcode_blind75/5_max_sum_subarray.py
@@ -1,5 +1,21 @@
 """Maximum Sum Subarray."""
 
 
+class Solution:
+    def maxSubArray(self, nums: list[int]) -> int:
+        maxSub = nums[0]
+        curSum = 0
+        for n in nums:
+            if curSum < 0:
+                curSum = 0
+            curSum += n
+            maxSub = max(maxSub, curSum)
+
+        return maxSub
+
+
 if __name__ == "__main__":
     nums = [-2, 1, -3, 4, -1, 2, 1, -5, 4]
+    # nums = [5, 4, -1, 7, 8]
+    solution = Solution()
+    print(solution.maxSubArray(nums))
