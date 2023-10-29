a992f678b6e74f8febf264385929a320395d0e5f
Kamrul Hasan <kamrul.h456@gmail.com>
2023-10-29 23:48:08 +0600

    climb stairs back

--- leetcode_blind75/15_climbing_stairs.py
+++ leetcode_blind75/15_climbing_stairs.py
@@ -1,29 +1,46 @@
+dp = [-1] * 10000
+
+
 class Solution:
-    def __init__(self):
-        self.dp = [-1] * 45
+    def coinChange(self, coins: list[int], amount: int) -> int:
+        dp = [amount + 1] * (amount + 1)
+        dp[0] = 0
 
-    def climbStairs(self, n: int) -> int:
-        one, two = 1, 1
-        for i in range(n - 1):
-            temp = one
-            one = one + two
-            two = temp
+        for a in range(1, amount + 1):
+            for c in coins:
+                if a - c >= 0:
+                    dp[a] = min(dp[a], 1 + dp[a - c])
 
-        return one
+        return dp[amount] if dp[amount] != amount + 1 else -1
 
-    def climbStairs1(self, n: int) -> int:
-        if self.dp[n] != -1:
-            return self.dp[n]
-        if n < 0:
+    def coin_change1(self, coins: list[int], amount: int) -> int:
+        if dp[amount] != -1:
+            return dp[amount]
+        if amount == 0:
             return 0
-        if n == 0:
-            return 1
-        self.dp[n] = self.climbStairs1(n - 1) + self.climbStairs1(n - 2)
-        return self.dp[n]
+        ans = 1e3 + 10
+        for coin in coins:
+            if amount - coin >= 0:
+                dp[amount] = min(ans, self.coin_change1(coins, amount - coin) + 1)
+        return dp[amount]
+
+
+def coin_change(coins, amount) -> int:
+    if amount == 0:
+        return 0
+    ans = 1e5 + 10
+    for coin in coins:
+        if amount - coin >= 0:
+            ans = min(ans, coin_change(coins, amount - coin) + 1)
+
+    return ans
 
 
 if __name__ == "__main__":
     solution = Solution()
-    n = 3
-    print(solution.climbStairs(n))
+    coins = [1, 2, 5]
+    amount = 40
+    # print(coin_change(coins, amount))
+    # print(solution.coinChange(coins, amount))
     # print(solution.climbStairs1(n))
+    print(solution.coin_change1(coins, amount))
