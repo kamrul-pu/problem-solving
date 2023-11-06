d87d93bebfc79d1bb5ed6676845def055a71df50
Kamrul Hasan <kamrul.h456@gmail.com>
2023-11-06 20:51:22 +0600

    longest consequtive sequence using set

--- data_structure/array_longest_consequtive_sequence.py
+++ data_structure/array_longest_consequtive_sequence.py
@@ -2,33 +2,51 @@
 
 
 class Solution:
-    def __init__(self, arr: list[int]) -> None:
-        self.arr = arr
-
-    def longest_consequtive_seqence(self) -> int:
+    def longest_consequtive_seqence(self, arr: list[int]) -> int:
         # sort the array
-        self.arr.sort()
+        n: int = len(arr)
+        arr.sort()
         longest: int = 1
         last_smaller: int = -12435436
         count: int = 0
-        for i in range(len(self.arr)):
-            if self.arr[i] - 1 == last_smaller:
+        for i in range(n):
+            if arr[i] - 1 == last_smaller:
                 count += 1
-                last_smaller = self.arr[i]
-            elif self.arr[i] != last_smaller:
+                last_smaller = arr[i]
+            elif arr[i] != last_smaller:
                 count = 1
-                last_smaller = self.arr[i]
+                last_smaller = arr[i]
             longest = max(longest, count)
 
         return longest
 
-    def print_arr(self) -> None:
-        print(self.arr)
+    def longest_consequtive_sequence1(self, arr: list[int]) -> int:
+        n: int = len(arr)
+        if n == 0:
+            return 0
+        longest: int = 1
+        # insert the unique element in the set
+        st = set(arr)
+        for val in st:
+            # if current - 1 not in the set then it is the start point
+            if val - 1 not in st:
+                x: int = val
+                ct: int = 1
+                while (x + 1) in st:
+                    x += 1
+                    ct += 1
+
+                longest = max(longest, ct)
+
+        return longest
+
+    def print_arr(self, arr) -> None:
+        print(arr)
 
 
 if __name__ == "__main__":
     arr = [102, 4, 100, 1, 101, 3, 2, 1, 1]
-    solution = Solution(arr)
-    solution.print_arr()
-    print(solution.longest_consequtive_seqence())
-    solution.print_arr()
+    solution = Solution()
+    solution.print_arr(arr)
+    print(solution.longest_consequtive_seqence(arr))
+    print(solution.longest_consequtive_sequence1(arr))
