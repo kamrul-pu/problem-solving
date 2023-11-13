4015568643ede3ba053393f5a7f50b4621f5e29d
Kamrul Hasan <kamrul.h456@gmail.com>
2023-11-13 23:32:05 +0600

    inversion count optimal with merge sort

--- data_structure/array_inversion_count.py
+++ data_structure/array_inversion_count.py
@@ -4,6 +4,7 @@
 class Solution:
     def __init__(self, arr: list[int] = []) -> None:
         self.arr = arr
+        self.count = 0
 
     def inversion_list(self) -> list[list[int]]:
         # TC = O(n^2) Space O(1)
@@ -27,6 +28,53 @@ class Solution:
 
         return cnt
 
+    def merge(self, arr: list[int], low: int, mid: int, high: int) -> int:
+        temp = []  # temporary array
+        left = low  # starting index of left half of arr
+        right = mid + 1  # starting index of right half of arr
+
+        cnt = 0  # Modification 1: cnt variable to count the pairs
+
+        # storing elements in the temporary array in a sorted manner
+        while left <= mid and right <= high:
+            if arr[left] <= arr[right]:
+                temp.append(arr[left])
+                left += 1
+            else:
+                temp.append(arr[right])
+                cnt += mid - left + 1  # Modification 2
+                right += 1
+
+        # if elements on the left half are still left
+        while left <= mid:
+            temp.append(arr[left])
+            left += 1
+
+        # if elements on the right half are still left
+        while right <= high:
+            temp.append(arr[right])
+            right += 1
+
+        # transfering all elements from temporary to arr
+        for i in range(low, high + 1):
+            arr[i] = temp[i - low]
+
+        return cnt  # Modification 3
+
+    def mergeSort(self, arr: list[int], low: int, high: int) -> int:
+        cnt = 0
+        if low >= high:
+            return cnt
+        mid = (low + high) // 2
+        cnt += self.mergeSort(arr, low, mid)  # left half
+        cnt += self.mergeSort(arr, mid + 1, high)  # right half
+        cnt += self.merge(arr, low, mid, high)  # merging sorted halves
+        return cnt
+
+    def inversion_count_optimal(self) -> int:
+        n = len(self.arr)
+        return self.mergeSort(self.arr, 0, n - 1)
+
     def print_arr(self) -> None:
         print(self.arr, sep=" ", end="\n")
 
@@ -37,3 +85,4 @@ if __name__ == "__main__":
     soltuion.print_arr()
     print(soltuion.inversion_list())
     print(soltuion.inversion_count())
+    print(soltuion.inversion_count_optimal())
