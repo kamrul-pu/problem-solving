dfdd8720adc79e6908cebfc0117b9188d73feb93
Kamrul Hasan <kamrul.h456@gmail.com>
2023-11-16 00:32:28 +0600

    small issue fixed

--- algorithm/prime_numbers.py
+++ algorithm/prime_numbers.py
@@ -32,7 +32,7 @@ is_prime[0] = is_prime[1] = False
 
 for i in range(2, int(N**0.5) + 1):
     if is_prime[i] == True:
-        for j in range(i * 2, N, i):
+        for j in range(i * 2, N + 1, i):
             is_prime[j] = False
 
 t: int = int(input("Number of test case: "))
