35694960ac9c8ba5557a0631daf476a6d44c1312
Kamrul Hasan <kamrul.h456@gmail.com>
2023-11-16 00:50:23 +0600

    optimal solution added

--- algorithm/prime_numbers.py
+++ algorithm/prime_numbers.py
@@ -6,37 +6,42 @@ def is_prime_brute_force(n: int) -> bool:
         return False
     # is_prime = True
     for i in range(2, n):
-        if n % 2 == 0:
+        if n % i == 0:
             return False
 
     return True
 
 
-def is_prime_better(n: int) -> bool:
-    if n < 2:
+def is_prime_optimal(n: int) -> bool:
+    ct: int = 0
+    if n == 1:
         return False
-    for i in range(2, int(math.sqrt(n)) + 1):
-        if n % i == 0:
+    if n == 2 or n == 3:
+        return True
+    for i in range(5, int(n * 0.5) + 1, 6):
+        ct += 1
+        if n % i == 0 or n % (i + 2) == 0:
             return False
 
-    return False
+    return True
 
 
-print(is_prime_brute_force(2))
-print(is_prime_brute_force(5))
+print(is_prime_optimal(199))
+# print(is_prime_brute_force(2))
+# print(is_prime_brute_force(5))
 
-# using sieve algorithm pre computation technique
-N: int = 100
-is_prime: list[bool] = [True] * N
-is_prime[0] = is_prime[1] = False
+# # using sieve algorithm pre computation technique
+# N: int = 100
+# is_prime: list[bool] = [True] * N
+# is_prime[0] = is_prime[1] = False
 
-for i in range(2, int(N**0.5) + 1):
-    if is_prime[i] == True:
-        for j in range(i * 2, N + 1, i):
-            is_prime[j] = False
+# for i in range(2, int(N**0.5) + 1):
+#     if is_prime[i] == True:
+#         for j in range(i * 2, N + 1, i):
+#             is_prime[j] = False
 
-t: int = int(input("Number of test case: "))
-while t:
-    x: int = int(input("Enter Number to check: "))
-    print(f"{x} is prime" if is_prime[x] else f"{x} is not prime")
-    t -= 1
+# t: int = int(input("Number of test case: "))
+# while t:
+#     x: int = int(input("Enter Number to check: "))
+#     print(f"{x} is prime" if is_prime[x] else f"{x} is not prime")
+#     t -= 1
