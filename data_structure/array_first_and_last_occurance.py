db879f6cd64873e85d8fb3725b908115d00adf30
Kamrul Hasan <kamrul.h456@gmail.com>
2023-11-16 22:05:01 +0600

    first and last occurance optimized solution

--- data_structure/array_first_and_last_occurance.py
+++ data_structure/array_first_and_last_occurance.py
@@ -16,6 +16,86 @@ class Solution:
 
         return [first, last]
 
+    def lower_bound(self, x: int) -> int:
+        n: int = len(self.arr)
+        ans: int = n
+        low: int = 0
+        high: int = n - 1
+        while low <= high:
+            mid = (low + high) // 2
+            if self.arr[mid] >= x:
+                high = mid - 1
+                ans = mid
+            else:
+                low = mid + 1
+
+        return ans
+
+    def upper_bound(self, x: int) -> int:
+        n: int = len(self.arr)
+        ans: int = n
+        low: int = 0
+        high: int = n - 1
+        while low <= high:
+            mid = (low + high) // 2
+            if self.arr[mid] > x:
+                ans = mid
+                high = mid - 1
+            else:
+                low = mid + 1
+        return ans
+
+    def first_last_occurance_by_lb_hb(self, x: int) -> list[int]:
+        n: int = len(self.arr)
+        lb = self.lower_bound(x)
+        if lb == n or self.arr[lb] != x:
+            return [-1, -1]
+        up = self.upper_bound(x)
+        return [lb, up - 1]
+
+    def find_first(self, x: int) -> int:
+        n: int = len(self.arr)
+        ans: int = -1
+        low: int = 0
+        high: int = n - 1
+
+        while low <= high:
+            mid = (low + high) // 2
+            if self.arr[mid] == x:
+                ans = mid
+                high = mid - 1
+            elif self.arr[mid] > x:
+                high = mid - 1
+            else:
+                low = mid + 1
+
+        return ans
+
+    def find_last(self, x: int) -> int:
+        n: int = len(self.arr)
+        ans: int = -1
+        low: int = 0
+        high: int = n - 1
+
+        while low <= high:
+            mid = (low + high) // 2
+            if self.arr[mid] == x:
+                ans = mid
+                low = mid + 1
+            elif self.arr[mid] > x:
+                high = mid - 1
+            else:
+                low = mid + 1
+
+        return ans
+
+    def first_last_occurance_2(self, x: int) -> int:
+        first: int = self.find_first(x)
+        if first == -1:
+            return [-1, -1]
+        last: int = self.find_last(x)
+        return [first, last]
+
     def print_arr(self) -> None:
         print(self.arr, sep=" ")
 
@@ -25,3 +105,7 @@ if __name__ == "__main__":
     soltuion = Solution(arr)
     soltuion.print_arr()
     print(soltuion.first_and_last_occurance(2))
+    print(soltuion.lower_bound(9))
+    print(soltuion.upper_bound(9))
+    print(soltuion.first_last_occurance_by_lb_hb(12))
+    print(soltuion.first_last_occurance_2(12))
