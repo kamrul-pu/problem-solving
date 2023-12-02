46e6a9214dcb1f175fbf5b26fc53fb90f838cca6
Kamrul Hasan <kamrul.h456@gmail.com>
2023-12-02 15:14:26 +0600

    width of any binray tree

--- data_structure/tree/bt_widht_of_bt.py
+++ data_structure/tree/bt_widht_of_bt.py
@@ -16,6 +16,7 @@ class TreeNode:
 def width_of_bt(root: TreeNode) -> int:
     if root is None:
         return 0
+    ans: int = 0
     q = deque()
     q.append((root, 0))
     while q:
@@ -23,15 +24,21 @@ def width_of_bt(root: TreeNode) -> int:
         mmin: int = q[0][1]
         first: int = 0
         last: int = 0
-        while cur_size > 0:
-            node: TreeNode = q.popleft()
+        for i in range(cur_size):
+            front = q.popleft()
+            cur_id: int = front[1] - mmin
+            node: TreeNode = front[0]
+            if i == 0:
+                first = cur_id
+            if i == cur_size - 1:
+                last = cur_id
             if node.left:
-                q.append(node.left)
+                q.append((node.left, cur_id * 2 + 1))
             if node.right:
-                q.append(node.right)
-            cur_size -= 1
+                q.append((node.right, cur_id * 2 + 2))
+        ans = max(ans, last - first + 1)
 
-    return
+    return ans
 
 
 def build_tree():
