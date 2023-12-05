e4d4195f334a04b47e98fb60b7a89e185b028bed
Kamrul Hasan <kamrul.h456@gmail.com>
2023-12-05 18:27:03 +0600

    test tree changed

--- data_structure/tree/bt_count_nodes.py
+++ data_structure/tree/bt_count_nodes.py
@@ -45,9 +45,13 @@ def build_tree():
     root.left = TreeNode(2)
     root.right = TreeNode(3)
     root.left.left = TreeNode(4)
-    root.left.left.right = TreeNode(7)
-    root.right.left = TreeNode(5)
-    root.right.right = TreeNode(6)
+    root.left.left.left = TreeNode(8)
+    root.left.left.right = TreeNode(9)
+    root.left.right = TreeNode(5)
+    root.left.right.left = TreeNode(10)
+    root.left.right.right = TreeNode(11)
+    root.right.left = TreeNode(6)
+    root.right.right = TreeNode(7)
 
     return root
 
