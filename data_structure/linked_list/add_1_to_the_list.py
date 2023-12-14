a8af5ab72ebab9752e2435fd6bdc5e069a666318
Kamrul Hasan <kamrul.h456@gmail.com>
2023-12-15 02:17:16 +0600

    add 1 to linked list

---
+++ data_structure/linked_list/add_1_to_the_list.py
@@ -0,0 +1,123 @@
+"""Check if a linked list is palindrome or not."""
+
+
+class Node:
+    def __init__(self, data: int) -> None:
+        self.data = data
+        self.next = None
+
+
+class SingleLinkedList:
+    def __init__(self) -> None:
+        self.head: Node = None
+        self.tail: Node = None
+
+    def add_data(self, data: int) -> None:
+        node: Node = Node(data)
+        if self.head is None:
+            self.head = node
+            self.tail = node
+            return
+        self.tail.next = node
+        self.tail = node
+
+    def check_palindrome(self) -> bool:
+        if self.head is None:
+            return True
+
+        lst: list[int] = []
+        temp: Node = self.head
+        while temp:
+            lst.append(temp.data)
+            temp = temp.next
+
+        left: int = 0
+        right: int = len(lst) - 1
+        while left < right:
+            if lst[left] != lst[right]:
+                return False
+            left += 1
+            right -= 1
+
+        return True
+
+    def print_list(self) -> None:
+        cur: Node = self.head
+        while cur:
+            print(cur.data, end="->")
+            cur = cur.next
+        print("Null")
+
+
+def reverse_list(head: Node) -> Node:
+    if head is None or head.next is None:
+        return head
+    temp: Node = head
+    prev: Node = None
+    while temp:
+        front: Node = temp.next
+        temp.next = prev
+        prev = temp
+        temp = front
+    # prev is the current head so return it
+    return prev
+
+
+def add_one(head: Node) -> Node:
+    if head is None:
+        return head
+    head = reverse_list(head=head)
+    carry: int = 1
+    temp: Node = head
+    while temp:
+        temp.data = temp.data + carry
+        if temp.data < 10:
+            carry = 0
+            break
+        else:
+            temp.data = 0
+            carry = 1
+        temp = temp.next
+
+    head = reverse_list(head)
+    if carry:
+        node = Node(carry)
+        node.next = head
+        head = node
+    return head
+
+
+def helper(node: Node) -> int:
+    if node is None:
+        return 1
+    carry: int = helper(node=node.next)
+    node.data = node.data + carry
+    if node.data < 0:
+        return 0
+    node.data = 0
+    return 1
+
+
+def add_one_optimal(head: Node) -> Node:
+    if head is None:
+        return head
+    carry: int = helper(head)
+    if carry == 1:
+        node = Node(carry)
+        node.next = head
+        head = node
+    return head
+
+
+if __name__ == "__main__":
+    llist: SingleLinkedList = SingleLinkedList()
+    print(llist.head)
+    llist.add_data(9)
+    llist.add_data(9)
+    llist.add_data(9)
+    llist.add_data(9)
+    llist.print_list()
+    # llist.head = add_one(head=llist.head)
+    # llist.print_list()
+    llist.head = add_one_optimal(head=llist.head)
+    llist.print_list()
