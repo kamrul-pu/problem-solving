cb6454d495afab8fabfa53fa6e0677c73989bf5d
Kamrul Hasan <kamrul.h456@gmail.com>
2023-12-19 23:43:07 +0600

    find first sub sequence:

--- python_basics/recursion/given_sum.py
+++ python_basics/recursion/given_sum.py
@@ -21,3 +21,31 @@ def given_sum(i: int, n: int, arr: list[int], sum: int, s) -> None:
 
 
 given_sum(0, len(nums), [], 2, 0)
+
+
+def given_sum2(i: int, n: int, arr: list[int], sum: int, s) -> bool:
+    """Find only one subsequence."""
+    if i == n:
+        # condition staiesfied
+        if s == sum:
+            print(arr)
+            return True
+        # condition not satisfied
+        return False
+    # take the element
+    arr.append(nums[i])
+    # add current one
+    s += nums[i]
+    if given_sum2(i + 1, n, arr, sum, s):
+        return True
+    # remove from list and reduce from some to give the clean list
+    arr.pop()
+    s -= nums[i]
+    # doest not take the element
+    if given_sum2(i + 1, n, arr, sum, s):
+        return True
+
+    return False
+
+
+print(given_sum2(0, len(nums), [], 2, 0))
