5a7c175b4a0103cf8cf2973989145d498420efa1
Kamrul Hasan <kamrul.h456@gmail.com>
2023-12-21 18:02:25 +0600

    suset sum unique

--- python_basics/recursion/subset_sum_2.py
+++ python_basics/recursion/subset_sum_2.py
@@ -2,7 +2,9 @@ def subset_generation(
     nums: list[int], n: int, i: int, subset: list[int], subsets: list[int]
 ) -> None:
     if i == n:
-        subsets.append(subset.copy())
+        data = sorted(subset.copy())
+        key = tuple(data)
+        subsets[key] = data
         return
     subset_generation(nums=nums, n=n, i=i + 1, subset=subset, subsets=subsets)
     subset.append(nums[i])
@@ -10,9 +12,26 @@ def subset_generation(
     subset.pop()
 
 
+def subset_generation_optimal(
+    nums: list[int], n: int, ind: int, subset: list[int], subsets: list[list[int]]
+) -> None:
+    subsets.append(subset.copy())
+    for i in range(ind, n):
+        if i != ind and nums[i] == nums[i - 1]:
+            continue
+        subset.append(nums[i])
+        subset_generation_optimal(
+            nums=nums, n=n, ind=i + 1, subset=subset, subsets=subsets
+        )
+        subset.pop()
+
+
 if __name__ == "__main__":
     nums: list[int] = [1, 2, 2]
     n: int = len(nums)
+    # subsets: list[list[int]] = {}
+    # subset_generation(nums=nums, n=n, i=0, subset=[], subsets=subsets)
+    # print(sorted(subsets.values()))
     subsets: list[list[int]] = []
-    subset_generation(nums=nums, n=n, i=0, subset=[], subsets=subsets)
+    subset_generation_optimal(nums=nums, n=n, ind=0, subset=[], subsets=subsets)
     print(subsets)
