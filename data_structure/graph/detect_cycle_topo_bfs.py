dd635200329683639c4ca4243c8e8d55cff8f336
Kamrul Hasan <kamrul.h456@gmail.com>
2023-12-29 13:10:17 +0600

    detect cycle using topo sort

---
+++ data_structure/graph/detect_cycle_topo_bfs.py
@@ -0,0 +1,60 @@
+"""
+Detect cycle using topological sort and Kahn's Algorithm.
+"""
+
+from collections import deque
+
+
+def topo_sort(g: list[list[int]], v: int) -> bool:
+    """
+    Perform topological sorting on a Directed Acyclic Graph (DAG) using BFS Kahn's Algorithm.
+
+    :param g: Adjacency list representing the directed acyclic graph.
+    :param v: Number of vertices in the graph.
+    :return: True if the graph contains a cycle, False otherwise.
+    """
+    # Initialize indegree array for each vertex
+    indegree: list[int] = [0] * v
+
+    # Calculate indegree for each vertex
+    for i in range(v):
+        for item in g[i]:
+            indegree[item] += 1
+
+    # Initialize a queue for BFS
+    q: deque = deque()
+
+    # Enqueue vertices with indegree 0
+    for i in range(v):
+        if indegree[i] == 0:
+            q.append(i)
+
+    # List to store the topological ordering
+    topo: list[int] = []
+
+    # BFS
+    while q:
+        node = q.popleft()
+        topo.append(node)
+
+        # Update indegree for adjacent vertices
+        for it in g[node]:
+            indegree[it] -= 1
+            if indegree[it] == 0:
+                q.append(it)
+
+    # If the length of the topological ordering is not equal to the number of vertices, a cycle exists
+    return not len(topo) == v
+
+
+if __name__ == "__main__":
+    # Example usage:
+    g: list[list[int]] = [
+        [1],
+        [2],
+        [3, 4],
+        [1],
+        [0],
+    ]
+    ans: bool = topo_sort(g=g, v=len(g))
+    print(ans)
