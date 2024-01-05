fb26f7dcac2a3b98c1d801282a8de060124020ba
Kamrul Hasan <kamrul.h456@gmail.com>
2024-01-05 18:29:36 +0600

    number of provinces using disjoint set

---
+++ data_structure/graph/number_of_provinces_ds.py
@@ -0,0 +1,75 @@
+"""Number of Provinces, graph using Disjoint Set."""
+
+
+class DSU:
+    def __init__(self, n: int) -> None:
+        # Initialize the Disjoint Set data structure with size and parent pointers.
+        self.size: list[int] = [1] * n
+        self.parent: list[int] = [0] * n
+        for i in range(n):
+            self.parent[i] = i
+
+    def find_parent(self, node: int) -> int:
+        """Find the parent of a node with path compression."""
+        if node == self.parent[node]:
+            return node
+        self.parent[node] = self.find_parent(self.parent[node])
+        return self.parent[node]
+
+    def union_by_size(self, u: int, v: int) -> None:
+        """Union by size to optimize the disjoint-set union operation."""
+        u_parent: int = self.find_parent(node=u)
+        v_parent: int = self.find_parent(node=v)
+
+        if u_parent == v_parent:
+            return
+
+        if self.size[u_parent] < self.size[v_parent]:
+            self.parent[u_parent] = v_parent
+            self.size[v_parent] += self.size[u_parent]
+        elif self.size[u_parent] > self.size[v_parent]:
+            self.parent[v_parent] = u_parent
+            self.size[u_parent] += self.size[v_parent]
+        else:
+            self.parent[u_parent] = v_parent
+            self.size[v_parent] += self.size[u_parent]
+
+
+def number_of_provinces(matrix: list[list[int]]) -> list[int]:
+    """Count the number of provinces using Disjoint Set."""
+    n: int = len(matrix)
+    ds: DSU = DSU(n=n)
+
+    # Union by size for connected nodes in the matrix.
+    for i in range(n):
+        for j in range(n):
+            if matrix[i][j] == 1:
+                ds.union_by_size(i, j)
+
+    provinces: list[int] = []
+    p_cnt: int = 0
+
+    # Collect unique parent nodes representing provinces.
+    for i in range(n):
+        # provinces.add(ds.parent[i])
+        if ds.parent[i] == i:
+            provinces.append(i)
+            p_cnt += 1
+
+    return provinces
+
+
+if __name__ == "__main__":
+    # Example usage
+    matrix: list[list[int]] = [
+        [0, 1, 0, 0, 0, 0, 0],
+        [1, 0, 1, 0, 0, 0, 0],
+        [0, 1, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 1, 0, 0],
+        [0, 0, 0, 1, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 1],
+        [0, 0, 0, 0, 0, 1, 0],
+    ]
+    provinces: set = number_of_provinces(matrix=matrix)
+    print(provinces)
+    print(len(provinces))
