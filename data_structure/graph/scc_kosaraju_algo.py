c33966d76f146586acf88a65ded360a238568925
Kamrul Hasan <kamrul.h456@gmail.com>
2024-01-07 16:47:53 +0600

    scc using kosaraju's algorithm

---
+++ data_structure/graph/scc_kosaraju_algo.py
@@ -0,0 +1,43 @@
+"""Strongly connected component using Kosaraju's algorithm."""
+
+
+def dfs(node: int, visited: list[bool], adj: list[list[int]], st: list[int]) -> None:
+    visited[node] = True
+
+    for item in adj[node]:
+        if not visited[item]:
+            dfs(node=item, visited=visited, adj=adj, st=st)
+
+    st.append(node)
+
+
+def kosaraju(v: int, adj: list[list[int]]) -> int:
+    visited: list[bool] = [False] * v
+    st: list[int] = []
+
+    for i in range(v):
+        if not visited[i]:
+            dfs(node=i, visited=visited, adj=adj, st=st)
+
+    # reverse the graph
+    adj_t: list[list[int]] = [[] for _ in range(v)]
+    for i in range(v):
+        visited[i] = False
+        for item in adj[i]:
+            adj_t[item].append(i)
+
+    # run dfs on nodes based on finishing time
+    scc: int = 0
+    while st:
+        node: int = st.pop()
+        if not visited[node]:
+            scc += 1
+            dfs(node=node, visited=visited, adj=adj_t, st=[])
+
+    return scc
+
+
+if __name__ == "__main__":
+    adj: list[list[int]] = [[1], [2], [0, 3], [4], [5, 7], [6], [4, 7], []]
+    scc: int = kosaraju(v=len(adj), adj=adj)
+    print(scc)
