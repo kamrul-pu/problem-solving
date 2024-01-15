ab3f47a7d9b29e641a416196e71103c0821aeef6
Kamrul Hasan <kamrul.h456@gmail.com>
2024-01-15 19:08:36 +0600

    transpose normal and trace of a matrix

--- python_basics/numpy/matrix_transpose.py
+++ python_basics/numpy/matrix_transpose.py
@@ -1,5 +1,6 @@
 """Transpose of a matrix."""
 import numpy as np
+import math
 
 if __name__ == "__main__":
     matrix = [[1, 2, 3], [4, 5, 6]]
@@ -8,3 +9,20 @@ if __name__ == "__main__":
     print(np.transpose(matrix))
     matrix = np.array([[1, 2, 3], [4, 5, 6]])
     print(matrix.T)
+    matrix: list[list[int]] = [
+        [7, 8, 9],
+        [6, 1, 2],
+        [5, 4, 3],
+    ]
+    n: int = len(matrix)
+    normal: int = 0
+    for i in range(n):
+        for j in range(n):
+            normal += matrix[i][j] * matrix[i][j]
+
+    normal = int(math.sqrt(normal))
+    print(normal)
+    trace: int = 0
+    for i in range(n):
+        trace += matrix[i][i]
+    print(trace)
