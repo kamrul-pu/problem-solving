cd358f5fcb7a424696ce43cf3b0fc581ffca7dd2
Kamrul Hasan <kamrul.h456@gmail.com>
2024-01-24 16:47:17 +0600

    count partition for given count

--- data_structure/dynamic_programming/18_count_partition_difference.py
+++ data_structure/dynamic_programming/18_count_partition_difference.py
@@ -1,4 +1,4 @@
-"""Count subsets with sum k."""
+"""Count partitions with given differencesubsets with sum k."""
 
 
 def generate_subset(arr: list[int], i: int, target: int, memo={}) -> int:
@@ -21,16 +21,25 @@ def generate_subset(arr: list[int], i: int, target: int, memo={}) -> int:
     return memo[key]
 
 
-def number_of_subset(arr: list[int], n: int, target: int) -> int:
-    return generate_subset(arr=arr, i=n - 1, target=target, memo={})
+def number_of_subset(arr: list[int], n: int, d: int) -> int:
+    total: int = sum(arr)
+    if total - d < 0 or (total - d) % 2 == 1:
+        return 0
+    return generate_subset(arr=arr, i=n - 1, target=(total - d) // 2, memo={})
 
 
-def number_of_subset_tabulation(arr: list[int], n: int, target: int) -> int:
+def number_of_subset_tabulation(arr: list[int], n: int, d: int) -> int:
+    total: int = sum(arr)
+    if total - d < 0 or (total - d) % 2 == 1:
+        return 0
+    target: int = (total - d) // 2
     dp: list[list[int]] = [[0 for col in range(target + 1)] for row in range(n)]
-    for i in range(n):
-        dp[i][0] = 1
+    if arr[0] == 0:
+        dp[0][0] = 2
+    else:
+        dp[0][0] = 1
 
-    if arr[0] <= target:
+    if arr[0] != 0 and arr[0] <= target:
         dp[0][arr[0]] = 1
 
     for i in range(1, n):
@@ -43,12 +52,19 @@ def number_of_subset_tabulation(arr: list[int], n: int, target: int) -> int:
     return dp[n - 1][target]
 
 
-def number_of_subset_optimal(arr: list[int], n: int, target: int) -> int:
+def number_of_subset_optimal(arr: list[int], n: int, d: int) -> int:
+    total: int = sum(arr)
+    if total - d < 0 or (total - d) % 2 == 1:
+        return 0
+    target: int = (total - d) // 2
     prev: list[int] = [0 for _ in range(target + 1)]
     cur: list[int] = [0 for _ in range(target + 1)]
-    cur[0] = 1
-    prev[0] = 1
-    if arr[0] <= target:
+    if arr[0] == 0:
+        prev[0] = 2
+    else:
+        prev[0] = 1
+
+    if arr[0] != 0 and arr[0] <= target:
         prev[arr[0]] = 1
 
     for i in range(1, n):
@@ -66,9 +82,9 @@ def number_of_subset_optimal(arr: list[int], n: int, target: int) -> int:
 
 
 if __name__ == "__main__":
-    arr: list[int] = [1, 2, 2, 3]
+    arr: list[int] = [5, 2, 6, 4]
     n: int = len(arr)
     target: int = 3
-    print(number_of_subset(arr=arr, n=n, target=target))
-    print(number_of_subset_tabulation(arr=arr, n=n, target=target))
-    print(number_of_subset_optimal(arr=arr, n=n, target=target))
+    print(number_of_subset(arr=arr, n=n, d=target))
+    print(number_of_subset_tabulation(arr=arr, n=n, d=target))
+    print(number_of_subset_optimal(arr=arr, n=n, d=target))
