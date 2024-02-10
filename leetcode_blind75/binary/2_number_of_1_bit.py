aa7339a39053301503b80a2036da142c84170058
Kamrul Hasan <kamrul.h456@gmail.com>
2024-02-10 23:33:27 +0600

    number of 1 bits in an integer with comment

--- leetcode_blind75/binary/2_number_of_1_bit.py
+++ leetcode_blind75/binary/2_number_of_1_bit.py
@@ -2,15 +2,19 @@
 
 
 class Solution:
+    # Function to count the number of '1' bits in the binary representation of the input integer
     def hammingWeight(self, n: int) -> int:
-        ans: int = 0
-        while n:
-            ans += n & 1
-            n >>= 1
-        return ans
+        ans: int = 0  # Initialize the count of '1' bits to 0
+        while n:  # Continue the loop until n becomes 0
+            ans += n & 1  # Increment the count if the least significant bit of n is '1'
+            n >>= 1  # Right shift n by 1 bit to check the next bit
+        return ans  # Return the total count of '1' bits
 
 
+# Main function to test the hammingWeight function
 if __name__ == "__main__":
-    n: int = 11
-    solution: Solution = Solution()
-    print(solution.hammingWeight(n=n))
+    n: int = 11  # Example input integer
+    solution: Solution = Solution()  # Create an instance of the Solution class
+    print(
+        solution.hammingWeight(n=n)
+    )  # Print the count of '1' bits in the binary representation of n
