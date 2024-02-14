2ee9e3a0f360cf5c12723fbf42174b0ec136c8a1
Kamrul Hasan <kamrul.h456@gmail.com>
2024-02-15 00:00:13 +0600

    merge two sorted list with comment

--- leetcode_blind75/linked_list/2_merge_two_sorted_list.py
+++ leetcode_blind75/linked_list/2_merge_two_sorted_list.py
@@ -14,68 +14,53 @@ class Solution:
     def __merge_optimal(
         self, list1: Optional[ListNode], list2: Optional[ListNode]
     ) -> Optional[ListNode]:
+        """
+        Merges two sorted linked lists in an optimal way.
+
+        Args:
+            list1 (Optional[ListNode]): Head of the first sorted linked list.
+            list2 (Optional[ListNode]): Head of the second sorted linked list.
+
+        Returns:
+            Optional[ListNode]: Head of the merged sorted linked list.
+        """
+        # If both lists are empty, return any of them (None in this case)
         if list1 is None and list2 is None:
             return list1
+        # If one list is empty, return the other list
         if list1 is None or list2 is None:
             return list1 if list2 is None else list2
 
+        # Determine the head of the merged list by selecting the smaller value
         seek, target = (list1, list2) if list1.val < list2.val else (list2, list1)
-
         head = seek
+
+        # Merge the lists by iterating through the nodes
         while seek and target:
+            # Move the seeker pointer until it's less than or equal to the target value
             while seek.next and seek.next.val < target.val:
                 seek = seek.next
 
+            # Swap pointers between seeker and target nodes
             seek.next, target = target, seek.next
             seek = seek.next
-
-        return head
-
-    def __merge(
-        self, list1: Optional[ListNode], list2: Optional[ListNode]
-    ) -> Optional[ListNode]:
-        if list1 is None and list2 is None:
-            return list1
-        if list1 is None or list2 is None:
-            return list1 if list2 is None else list2
-
-        temp1: ListNode = list1
-        temp2: ListNode = list2
-        if temp1.val < temp2.val:
-            new_list: ListNode = temp1
-            temp1 = temp1.next
-        else:
-            new_list = temp2
-            temp2 = temp2.next
-        head: ListNode = new_list
-        cur: ListNode = new_list
-        while temp1 and temp2:
-            if temp1.val < temp2.val:
-                cur.next = temp1
-                temp1 = temp1.next
-                cur = cur.next
-            else:
-                cur.next = temp2
-                temp2 = temp2.next
-                cur = cur.next
-
-        while temp1:
-            cur.next = temp1
-            temp1 = temp1.next
-            cur = cur.next
-
-        while temp2:
-            cur.next = temp2
-            temp2 = temp2.next
-            cur = cur.next
 
         return head
 
     def mergeTwoLists(
         self, list1: Optional[ListNode], list2: Optional[ListNode]
     ) -> Optional[ListNode]:
+        """
+        Public interface for merging two sorted linked lists.
+
+        Args:
+            list1 (Optional[ListNode]): Head of the first sorted linked list.
+            list2 (Optional[ListNode]): Head of the second sorted linked list.
+
+        Returns:
+            Optional[ListNode]: Head of the merged sorted linked list.
+        """
         return self.__merge_optimal(list1, list2)
-        # return self.__merge(list1=list1, list2=list2)
 
     def print_list(self, head: Optional[ListNode]) -> None:
         cur: ListNode = head
