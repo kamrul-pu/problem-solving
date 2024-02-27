6a366853efa72f5c5b2a2c902770c4813ad8c0fd
Kamrul Hasan <kamrul.h456@gmail.com>
2024-02-27 22:07:31 +0600

    word ladder optimized solution

--- leetcode_blind75/graph/27_word_ladder_2.py
+++ leetcode_blind75/graph/27_word_ladder_2.py
@@ -11,12 +11,69 @@ or an empty list if no such sequence exists. Each sequence should be returned as
 [beginWord, s1, s2, ..., sk].
 """
 
-from collections import deque
-from typing import List, Deque, Set
+from collections import defaultdict, deque
+from typing import List, Deque, Set, DefaultDict, Dict
 
 
 class Solution:
-    def findLadders(
+    # mpp: DefaultDict = defaultdict(int)
+
+    def __dfs(
+        self,
+        word: str,
+        sequence: List[str],
+        beginWord: str,
+        ans: List[List[str]],
+        mpp: Dict,
+    ) -> None:
+        if word == beginWord:
+            ans.append(list(reversed(sequence)))
+            return
+        steps: int = mpp[word]
+        for i in range(len(word)):
+            for ch in range(ord("a"), ord("z") + 1):
+                new_word: str = word[:i] + chr(ch) + word[i + 1 :]
+                if new_word in mpp and mpp[new_word] + 1 == steps:
+                    sequence.append(new_word)
+                    self.__dfs(new_word, sequence, beginWord, ans, mpp)
+                    sequence.pop()
+
+    def __find_ladders_optimal(
+        self, beginWord: str, endWord: str, wordList: List[str]
+    ) -> List[List[str]]:
+        mpp: Dict = dict()
+        # Create a set for quick word lookup
+        word_set: Set = set(wordList)
+        q: Deque = deque()
+        q.append(beginWord)
+        mpp[beginWord] = 1
+        n: int = len(beginWord)
+        if beginWord in word_set:
+            word_set.remove(beginWord)
+        while q:
+            word: str = q.popleft()
+            steps: int = mpp[word]
+            if word == endWord:
+                break
+            # n: int = len(word)
+            # Generate new words by changing one letter at a time
+            for i in range(n):
+                for ch in range(ord("a"), ord("z") + 1):
+                    new_word: str = word[:i] + chr(ch) + word[i + 1 :]
+                    if new_word in word_set:
+                        q.append(new_word)
+                        word_set.remove(new_word)
+                        mpp[new_word] = steps + 1
+        ans: List[List[str]] = []
+        if endWord in mpp:
+            sequence: List[str] = []
+            sequence.append(endWord)
+            self.__dfs(
+                word=endWord, sequence=sequence, beginWord=beginWord, ans=ans, mpp=mpp
+            )
+        return ans
+
+    def __find_ladders(
         self, beginWord: str, endWord: str, wordList: List[str]
     ) -> List[List[str]]:
         # Create a set for quick word lookup
@@ -75,11 +132,22 @@ class Solution:
 
         return ans
 
+    def findLadders(
+        self, beginWord: str, endWord: str, wordList: List[str]
+    ) -> List[List[str]]:
+        # return self.__find_ladders(
+        #     beginWord=beginWord, endWord=endWord, wordList=wordList
+        # )
+        return self.__find_ladders_optimal(
+            beginWord=beginWord, endWord=endWord, wordList=wordList
+        )
+
 
 if __name__ == "__main__":
     word_list: List[str] = ["hot", "dot", "dog", "lot", "log", "cog"]
     begin_word: str = "hit"
     end_word: str = "cog"
+    word_list = ["hot", "dot", "dog", "lot", "log"]
     solution: Solution = Solution()
 
     # Print the shortest transformation sequences from beginWord to endWord
