155de1cc1edf7ccfd5642efa2ce5e19404edb7bd
Kamrul Hasan <kamrul.h456@gmail.com>
2024-03-17 23:15:23 +0600

    successor and predicessor in bst

--- data_structure_and_algorithms/tree/40_bst_successor_predecessor.py
+++ data_structure_and_algorithms/tree/40_bst_successor_predecessor.py
@@ -1,92 +1,100 @@
 """Successors predecessor of Binary Search Tree"""
 
+from typing import Optional
+
 
 class Node:
-    def __init__(self, data: int) -> None:
-        self.data = data
+    def __init__(self, val) -> None:
+        """
+        Initializes a node of the binary tree.
+
+        Parameters:
+            val (int): The value of the node.
+        """
+        self.val = val
         self.left = None
         self.right = None
 
 
-def insert_node(root: Node, data: int) -> Node:
-    """Iterative approach."""
-    if root is None:
-        return Node(data)
+class Solution:
+    def get_successor(self, root: Optional[Node], key: int) -> Node:
+        """
+        Finds the successor node of a given key in the binary tree.
 
-    cur: Node = root
-    while True:
-        if cur.data <= data:
-            # insert in the right
-            if cur.right:
-                cur = cur.right
+        Parameters:
+            root (Optional[Node]): The root node of the binary tree.
+            key (int): The value whose successor is to be found.
+
+        Returns:
+            Optional[Node]: The successor node of the given key.
+        """
+        if root is None:
+            return None
+        successor: Node = None
+        curr: Node = root
+        while curr:
+            if curr.val > key:
+                # If current node's value is greater than the key, move left
+                successor = curr
+                curr = curr.left
             else:
-                cur.right = Node(data)
-                break
-        else:
-            # insert in the left subtree
-            if cur.left:
-                cur = cur.left
+                # If current node's value is less than or equal to the key, move right
+                curr = curr.right
+        return successor
+
+    def get_predicessor(self, root: Optional[Node], key: int) -> Node:
+        """
+        Finds the predecessor node of a given key in the binary tree.
+
+        Parameters:
+            root (Optional[Node]): The root node of the binary tree.
+            key (int): The value whose predecessor is to be found.
+
+        Returns:
+            Optional[Node]: The predecessor node of the given key.
+        """
+        if root is None:
+            return None
+        predicessor: Node = None
+        curr: Node = root
+        while curr:
+            if curr.val < key:
+                # If current node's value is less than the key, move right
+                predicessor = curr
+                curr = curr.right
             else:
-                cur.left = Node(data)
-                break
+                # If current node's value is greater than or equal to the key, move left
+                curr = curr.left
+        return predicessor
 
+
+def build_tree() -> Node:
+    """
+    Helper method to build a sample binary tree for testing.
+
+    Returns:
+        Node: The root node of the constructed binary tree.
+    """
+    root: Node = Node(5)
+    root.left = Node(3)
+    root.right = Node(6)
+    root.left.left = Node(2)
+    root.left.right = Node(4)
+    root.right.right = Node(7)
     return root
-
-
-def in_order(root: Node) -> None:
-    if root is None:
-        return
-    in_order(root.left)
-    print(root.data, end="->")
-    in_order(root.right)
-
-
-def build_tree(elements: list[int]) -> Node:
-    root = Node(elements[0])
-    for i in range(1, len(elements)):
-        insert_node(root=root, data=elements[i])
-
-    return root
-
-
-def find_successor(root: Node, data: int) -> int:
-    if root is None:
-        return None
-    successor: int = None
-    curr: Node = root
-    while curr:
-        if curr.data > data:
-            # successors may be in the left portion
-            successor = curr.data
-            curr = curr.left
-        else:
-            # successors may be in the right subtree
-            curr = curr.right
-
-    return successor
-
-
-def find_predicessor(root: Node, data: int) -> int:
-    if root is None:
-        return None
-    predecessor: int = None
-    curr: Node = root
-    while curr:
-        if curr.data < data:
-            # successors may be in the right portion
-            predecessor = curr.data
-            curr = curr.right
-        else:
-            # successors may be in the left subtree
-            curr = curr.left
-
-    return predecessor
 
 
 if __name__ == "__main__":
-    elements: list[int] = [8, 5, 1, 7, 10, 12]
-    root: Node = build_tree(elements=elements)
-    in_order(root)
-    print("None")
-    print(find_successor(root=root, data=10))
-    print(find_predicessor(root=root, data=10))
+    # Build the binary search tree
+    root: Node = build_tree()
+
+    # Create an instance of Solution class
+    solution: Solution = Solution()
+
+    # Find the successor and predecessor nodes for the key value
+    successor: Node = solution.get_successor(root=root, key=60)
+    predicessor: Node = solution.get_predicessor(root=root, key=60)
+
+    # Print the values of the successor and predecessor nodes
+    print(successor.val if successor else "Null")
+    print(predicessor.val if predicessor else "Null")
