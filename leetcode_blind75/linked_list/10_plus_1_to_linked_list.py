c1a22ea0f886c445ec9b51d00c9df603c8df8349
Kamrul Hasan <kamrul.h456@gmail.com>
2024-03-23 15:14:57 +0600

    plus 1 to linked list

--- leetcode_blind75/linked_list/10_plus_1_to_linked_list.py
+++ leetcode_blind75/linked_list/10_plus_1_to_linked_list.py
@@ -42,24 +42,36 @@ class Solution:
         Returns:
             Node: The head of the updated linked list representing the incremented number.
         """
+        # Reverse the linked list to simplify addition
         new_head = self.__reverse(head=head)
+
+        # Initialize carry for addition
         carry: int = 1
         temp = new_head
         while temp:
+            # Add carry to the current node's data
             temp.data = temp.data + carry
+
+            # Check if the result is less than 10
             if temp.data < 10:
-                carry = 0
+                carry = 0  # Update carry to 0
                 break
             else:
-                carry = 1
-                temp.data = 0
+                carry = 1  # Update carry to 1 for next iteration
+                temp.data = 0  # Update current node's data to 0 for single-digit result
+
+            # Move to the next node
             temp = temp.next
 
+        # Reverse the linked list back to its original order
         head: Node = self.__reverse(head=new_head)
+
+        # If carry is still 1, create a new node with value 1 and append it to the head
         if carry:
             node = Node(carry)
             node.next = head
             head = node
+
         return head
 
     def addOne(self, head: Node) -> Node:
