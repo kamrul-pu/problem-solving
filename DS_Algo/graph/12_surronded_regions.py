af6ae67c3c63a33f03b19ee58877ec38e5217dd5
Kamrul Hasan <kamrul.h456@gmail.com>
2024-05-20 22:46:55 +0600

    c:/Users/Admin/Desktop/problem-solving/venv/Scripts/python.exe c:/Users/Admin/Desktop/problem-solving/DS_Algo/graph/12_surronded_regions.py

--- DS_Algo/graph/12_surronded_regions.py
+++ DS_Algo/graph/12_surronded_regions.py
@@ -1,79 +1,86 @@
-"""Surrounded Regions Replace 0's with X's."""
+"""
+Given an m x n matrix board containing 'X' and 'O', capture all regions that are 4-directionally surrounded by 'X'.
 
-from typing import List
+A region is captured by flipping all 'O's into 'X's in that surrounded region.
+"""
 
-
-def dfs(
-    row: int, col: int, n: int, m: int, g: List[List[int]], visited: List[List[bool]]
-) -> None:
-    """Depth-First Search to mark connected 'O' cells as visited."""
-    visited[row][col] = True
-    # Define directions for movement: up, right, down, left
-    dr: List[int] = [-1, 0, 1, 0]
-    dc: List[int] = [0, 1, 0, -1]
-
-    for i in range(len(dr)):
-        nrow: int = row + dr[i]
-        ncol: int = col + dc[i]
-
-        # Check if the neighboring cell is within bounds, unvisited, and contains 'O'
-        if (
-            nrow >= 0
-            and nrow < n
-            and ncol >= 0
-            and ncol < m
-            and not visited[nrow][ncol]
-            and g[nrow][ncol] == "O"
-        ):
-            # Recursively call DFS for the neighboring 'O' cell
-            dfs(row=nrow, col=ncol, n=n, m=m, g=g, visited=visited)
+from collections import deque
+from typing import Deque, List, Tuple
 
 
-def replace_0s(g: List[List[str]], n: int, m: int) -> None:
-    """Replace surrounded 'O' cells with 'X'."""
-    visited: List[List[bool]] = [[False for col in range(m)] for row in range(n)]
+class Solution:
+    def __bfs(self, board: List[List[str]]) -> None:
+        # Define rows and cols of the board
+        rows, cols = len(board), len(board[0])
+        # Initialize a 2D visited array to keep track of visited cells
+        visited: List[List[bool]] = [[False] * cols for _ in range(rows)]
+        # Initialize a deque for BFS traversal
+        q: Deque[Tuple[int]] = deque()
 
-    # Visit top and bottom boundary
-    for j in range(m):
-        if g[0][j] == "O" and not visited[0][j]:
-            dfs(row=0, col=j, n=n, m=m, g=g, visited=visited)
-        if g[n - 1][j] == "O" and not visited[n - 1][j]:
-            dfs(row=n - 1, col=j, n=n, m=m, g=g, visited=visited)
+        # Check the left and right edges of the board
+        for row in range(rows):
+            if board[row][0] == "O" and not visited[row][0]:
+                q.append((row, 0))
+                visited[row][0] = True
+            if board[row][cols - 1] == "O" and not visited[row][cols - 1]:
+                q.append((row, cols - 1))
+                visited[row][cols - 1] = True
 
-    # Visit the left and right boundary
-    for i in range(n):
-        if g[i][0] == "O" and not visited[i][0]:
-            dfs(row=i, col=0, n=n, m=m, g=g, visited=visited)
-        if g[i][m - 1] == "O" and not visited[i][m - 1]:
-            dfs(row=i, col=m - 1, n=n, m=m, g=g, visited=visited)
+        # Check the top and bottom edges of the board
+        for col in range(cols):
+            if board[0][col] == "O" and not visited[0][col]:
+                q.append((0, col))
+                visited[0][col] = True
+            if board[rows - 1][col] == "O" and not visited[rows - 1][col]:
+                q.append((rows - 1, col))
+                visited[rows - 1][col] = True
 
-    # Replace unvisited 'O' cells with 'X'
-    for i in range(n):
-        for j in range(m):
-            if not visited[i][j] and g[i][j] == "O":
-                g[i][j] = "X"
+        # Define direction arrays for BFS
+        del_row: List[int] = [-1, 0, 1, 0]
+        del_col: List[int] = [0, 1, 0, -1]
+
+        # Perform BFS traversal
+        while q:
+            row, col = q.popleft()
+            for i in range(4):
+                nrow: int = row + del_row[i]
+                ncol: int = col + del_col[i]
+                # Check if the neighboring cell is valid and unvisited
+                if (
+                    rows > nrow >= 0
+                    and cols > ncol >= 0
+                    and board[nrow][ncol] == "O"
+                    and not visited[nrow][ncol]
+                ):
+                    # Mark the neighboring cell as visited and add it to the queue
+                    q.append((nrow, ncol))
+                    visited[nrow][ncol] = True
+
+        # After BFS traversal, mark all unvisited 'O's as 'X's
+        for row in range(rows):
+            for col in range(cols):
+                if not visited[row][col] and board[row][col] == "O":
+                    board[row][col] = "X"
+
+    def solve(self, board: List[List[str]]) -> None:
+        """
+        Do not return anything, modify board in-place instead.
+        """
+        # Call the BFS function
+        self.__bfs(board=board)
 
 
 if __name__ == "__main__":
-    # Example usage with a 2D grid
-    g: List[List[str]] = [
-        ["X", "X", "X", "X", "X"],
-        ["X", "O", "O", "X", "O"],
-        ["X", "X", "O", "X", "O"],
-        ["X", "O", "X", "O", "X"],
-        ["O", "O", "X", "X", "X"],
+    # Example board
+    board: List[List[str]] = [
+        ["X", "X", "X", "X"],
+        ["X", "O", "O", "X"],
+        ["X", "X", "O", "X"],
+        ["X", "O", "X", "X"],
     ]
-    n: int = len(g)
-    m: int = len(g[0])
-
-    # Print the original grid
-    for row in g:
-        print(row)
-    print("=========================")
-
-    # Replace surrounded 'O' cells with 'X'
-    replace_0s(g=g, n=n, m=m)
-
-    # Print the modified grid
-    for row in g:
-        print(row)
+    # Initialize solution object
+    solution: Solution = Solution()
+    # Solve the problem
+    solution.solve(board=board)
+    # Print the modified board
+    print(board)
