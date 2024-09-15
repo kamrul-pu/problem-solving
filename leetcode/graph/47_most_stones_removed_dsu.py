17e642d684d8fe438ace83fcd6607cf223ab35f8
Kamrul Hasan <kamrul.h456@gmail.com>
2024-09-15 14:59:07 +0600

    most stones removed solution using dsu

--- leetcode/graph/47_most_stones_removed_dsu.py
+++ leetcode/graph/47_most_stones_removed_dsu.py
@@ -82,60 +82,9 @@ class Solution:
         # The number of stones that can be removed is total stones minus the number of unique components
         return n - cnt
 
-    def removeStones(self, stones: List[List[int]]) -> int:
-        graphR = {}
-        graphC = {}
-        for stone in stones:
-            if stone[0] not in graphR:
-                graphR[stone[0]] = [stone[1]]
-
-            else:
-                graphR[stone[0]].append(stone[1])
-
-            if stone[1] not in graphC:
-                graphC[stone[1]] = [stone[0]]
-            else:
-                graphC[stone[1]].append(stone[0])
-        print(graphR, graphC)
-
-        def bfs(node, visited):
-            # visited[node] = 1
-            # if node not in graph:
-            #     return 0
-            q = [node]
-            # rows = []
-            removedStones = 0  # len(graph[node])
-            while q:
-                # print(q)
-                currRow = q.pop()
-                if currRow in visited:
-                    continue
-                # rows.append(currRow)
-                removedStones += len(graphR[currRow])
-                visited[currRow] = 1
-                for col in graphR[currRow]:
-                    # if col not in graphC:
-                    #     continue
-                    for row in graphC[col]:
-                        if row not in visited:
-                            q.append(row)
-            return removedStones
-
-        ans = 0
-        visited = {}
-        # covered = {}
-        for key in graphR.keys():
-            if key not in visited:
-                removedStones = bfs(key, visited) - 1
-                ans += removedStones
-
-        return ans
-
 
 if __name__ == "__main__":
+    n: int = 6
     stones: List[List[int]] = [[0, 0], [0, 1], [1, 0], [1, 2], [2, 1], [2, 2]]
-    stones = [[0, 0], [0, 2], [1, 1], [2, 0], [2, 2]]
-    stones = [[0, 0]]
-    stones = [[0, 1], [1, 0]]
     solution: Solution = Solution()
-    print(solution.removeStones(stones=stones))
+    print(solution.maxRemove(stones, n))
